824e84df99a2dd28b8e00711b36fef1b353b45ed
Kevin Blair <kblair40@gmail.com>
2021-07-11 07:15:40 -0500

    Add slide transition for welcome page

--- components/Avatar/Background/Backdrop.js
+++ components/Avatar/Background/Backdrop.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Slide from "@material-ui/core/Slide";
 import classes from "./Backdrop.module.css";
 
 const Backdrop = (props) => {
   return (
-    <div className={classes.backdropContainer}>
-      <div className={classes.circle}>{props.children}</div>
-    </div>
+    <Slide in={true} direction="down" timeout={500}>
+      <div className={classes.backdropContainer}>
+        <div className={classes.circle}>{props.children}</div>
+      </div>
+    </Slide>
   );
 };
 
