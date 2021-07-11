3d55a16efd6a934533aa683b2e005186bdc93bef
Kevin Blair <kblair40@gmail.com>
2021-07-11 12:28:57 -0500

    Add Intro

--- components/Content/HomeContent.js
+++ components/Content/HomeContent.js
@@ -5,11 +5,14 @@ const HomeContent = () => {
   return (
     <div className={classes.container}>
       <div className={classes.textContainer}>
-        <div className={classes.textColumn}>
-          <h1 className="nameIntro">Kevin Blair</h1>
+        <div className={classes.textSection}>
+          <h1 className={classes.nameIntro}>Kevin Blair</h1>
         </div>
-        <div className={classes.textColumn}>
-          <p>Quis laboris Lorem ad eiusmod quis amet amet dolore Lorem.</p>
+        <div className={classes.textSection}>
+          <p>
+            Quis laboris Lorem ad eiusmod quis amet amet dolore Lorem. Quis
+            laboris Lorem ad eiusmod quis amet amet dolore Lorem.
+          </p>
         </div>
       </div>
     </div>
